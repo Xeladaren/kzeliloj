aae1a7cb5c4c9c04219b0b9c8a1c8b40b8323a39
Xeladaren <xeladaren@pm.me>
2021-06-23 20:39:00 +0200

    add assert define in 'kzeliloj/debug.h'

--- include/kzeliloj/debug.h
+++ include/kzeliloj/debug.h
@@ -1,7 +1,9 @@
-#include <stdio.h>
 
-#ifndef PRINT_DEBUG
-#define PRINT_DEBUG
+#ifndef PRINT_DEBUG_H
+#define PRINT_DEBUG_H
+
+#include <errno.h>
+#include <stdio.h>
 
 #ifdef DEBUG
 #define PRINT_DEBUG(str, ...)   fprintf(stdout, "[\033[35;1mDEBUG\033[0m] %s:%d : " str "\n", __func__, __LINE__, ##__VA_ARGS__)
@@ -15,5 +17,28 @@
 #define PRINT_ERROR(str, ...)   fprintf(stderr, "[\033[31;1mERROR\033[0m] " str "\n", ##__VA_ARGS__)
 #endif
 
+#define ASSERT(cond, jumpLabel) \
+if(!(cond)) { \
+    goto jumpLabel; \
+}
 
-#endif
+#define ASSERT_PRINT(cond, jumpLabel, msg) \
+if(!(cond)) { \
+    perror(msg); \
+    goto jumpLabel; \
+}
+
+#define ASSERT_ERROR(cond, jumpLabel, err) \
+if(!(cond)) { \
+    errno = err; \
+    goto jumpLabel; \
+}
+
+#define ASSERT_PRINT_ERROR(cond, jumpLabel, err, msg) \
+if(!(cond)) { \
+    errno = err; \
+    perror(msg); \
+    goto jumpLabel; \
+}
+
+#endif /* PRINT_DEBUG_H */
